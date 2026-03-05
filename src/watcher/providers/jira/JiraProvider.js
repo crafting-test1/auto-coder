53eaf7bd00ef131753d233f7d29537551829397f
Yuan <whywhy36@gmail.com>
2026-03-05 14:27:26 +1300

    Created JiraProvider.js with the base structure.

---
+++ src/watcher/providers/jira/JiraProvider.js
@@ -0,0 +1,10 @@
+// Base structure for the JiraProvider
+class JiraProvider {
+  constructor() {
+    // Initialize Jira provider
+  }
+
+  // Define your methods here
+}
+
+export default JiraProvider;
